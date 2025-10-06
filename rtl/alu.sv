a0b2e6c7c74fffc133d1f1591d7454d92c4eda27
23060124-Xu zhi <2795707251@qq.com>
2025-10-06 09:20:07 +0200

    finish milestone 1

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,15 +1,38 @@
 module alu #(
   BW = 16 // bitwidth
   ) (
-  input  logic unsigned [BW-1:0] in_a,
-  input  logic unsigned [BW-1:0] in_b,
-  input  logic             [3:0] opcode,
-  output logic unsigned [BW-1:0] out,
-  output logic             [2:0] flags // {overflow, negative, zero}
+  input  logic signed  [BW-1:0] in_a,
+  input  logic signed  [BW-1:0] in_b,
+  input  logic         [3:0] opcode,
+  output logic signed  [BW-1:0] out,
+  output logic         [2:0] flags // {overflow, negative, zero}
   );
 
+logic [BW-1:0] res;
   // Complete your RTL code here
+always_comb begin
+  case (opcode)
+    3'b000: res = in_a + in_b;
+    3'b001: res = in_a - in_b; 
+    3'b010: res = in_a && in_b; //logic and
+    3'b011: res = in_a || in_b;
+    3'b100: res = in_a ^ in_b;
+    3'b101: res = in_a + 1;
+    3'b110: res = in_a;
+    3'b111: res = in_b;
+    default: res = 0;
+  endcase
+end
 
+assign out = res;
+wire overflow, negative, zero;
+assign flags = {overflow, negative, zero};
+
+assign overflow = (opcode == 3'b000 && ((in_a > 0 && in_b > 0 && res < 0) || (in_a < 0 && in_b < 0 && res > 0))) ||
+                  (opcode == 3'b001 && ((in_a > 0 && in_b < 0 && res < 0) || (in_a < 0 && in_b > 0 && res > 0)));
+
+assign negative = res < 0;
+assign zero = (res == 0);
 endmodule
 
 
--- tb/alu_tb.sv
+++ tb/alu_tb.sv
@@ -1,11 +1,10 @@
 module alu_tb;
 
-  parameter BW = 16; // bitwidth
-
-  logic unsigned [BW-1:0] in_a;
-  logic unsigned [BW-1:0] in_b;
+  localparam BW = 16; // bitwidth
+  logic signed [BW-1:0] in_a;
+  logic signed [BW-1:0] in_b;
   logic             [3:0] opcode;
-  logic unsigned [BW-1:0] out;
+  logic signed [BW-1:0] out;
   logic             [2:0] flags; // {overflow, negative, zero}
 
   // Instantiate the ALU
@@ -16,6 +15,14 @@ module alu_tb;
     .out(out),
     .flags(flags)
   );
+  
+  initial begin
+    // use iverilog to create a waveform file
+    $dumpfile("alu_tb.vcd");
+    $dumpvars(0, alu_tb);
+    #1000ns;
+    $finish;
+  end
 
   // Generate stimuli to test the ALU
   initial begin
@@ -24,5 +31,13 @@ module alu_tb;
     opcode = '0;
     #10ns;
     // Complete your testbench code here
+    in_a = 16'sd10000; in_b = 16'sd20000; opcode = 4'b0000; #10ns; // Addition
+    in_a = 16'sd30000; in_b = 16'sd10000; opcode = 4'b0001; #10ns; // Subtraction
+    in_a = 16'sd15; in_b = 16'sd27; opcode = 4'b0010; #10ns; // Logical AND
+    in_a = 16'sd15; in_b = 16'sd27; opcode = 4'b0011; #10ns; // Logical OR
+    in_a = 16'sd29; in_b = 16'sd15; opcode = 4'b0100; #10ns; // Bitwise XOR
+    in_a = 16'sd42; opcode = 4'b0101; #10ns; // Increment
+    in_a = 16'sd12345; opcode = 4'b0110; #10ns; // Pass through A
   end
+
 endmodule
